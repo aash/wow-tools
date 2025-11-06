eaa90c4bcb25b06a5eb1ede501f46a54ec802be6
Aleksei Shpitalev <aleksei.shpitalev@gmail.com>
2025-11-07 03:54:38 +0700

    new ahk version outputs 10-ary win id

--- win_tools.py
+++ win_tools.py
@@ -5,7 +5,7 @@ from common import Rect
 
 def get_window_rect(ahk: ahk.AHK, window_name: str) -> Rect:
     window = ahk.find_window(title=window_name)
-    window_id = int(window.id, 16)
+    window_id = int(window.id, 10)
     window.activate()
     client_area_zero = win32gui.ClientToScreen(window_id, (0,0))
     cr = win32gui.GetClientRect(window_id)
