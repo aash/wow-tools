a35ed8bb8dffb76aa2184e12ee38fa91f3586413
Aleksei Shpitalev <aleksei.shpitalev@gmail.com>
2025-11-07 03:57:05 +0700

    dummy implementation

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from overlay_client import overlay_client
+import time
+import numpy as np
+
+
 def main():
-    print("Hello from wow-tools!")
+    with overlay_client() as ovl_clt:
+        ovl_clt(np.array([[(0,0,0)]], dtype=np.uint8))
+        time.sleep(10)
 
 
 if __name__ == "__main__":
